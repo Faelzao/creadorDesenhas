8758f7f2f0d4dffcfbc1da69cabe83753fd439ce
Faelzao <rrafa8982@gmail.com>
2024-09-02 17:52:16 +0000

    save

--- geradorSenhas/App.js
+++ geradorSenhas/App.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import { ModalPassaword } from './src/components/modal/index.js';
 
 let charset = "abcdefghijklmnopqrstuvwxyz!#$&%0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 export default function App() {
-const [senhaGerada, setSenhaGerada] = useState("");
-  function gerarSenha(){
+  const [senhaGerada, setSenhaGerada] = useState("");
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function gerarSenha() {
     let senha = "";
-    for(let i = 0, n = charset.length; i < 8; i++){
-      senha+= charset.charAt(Math.floor(Math.random()* n))
+    for (let i = 0, n = charset.length; i < 8; i++) {
+      senha += charset.charAt(Math.floor(Math.random() * n))
     }
     setSenhaGerada(senha)
+    setModalVisible(true)
   }
   return (
     <View style={styles.container}>
@@ -21,9 +25,12 @@ const [senhaGerada, setSenhaGerada] = useState("");
       <Text style={styles.title}>LockGen</Text>
 
       <TouchableOpacity style={styles.button} onPress={gerarSenha}>
-      <Text style={styles.textBtn}>Gerar Senha</Text>
+        <Text style={styles.textBtn}>Gerar Senha</Text>
 
       </TouchableOpacity>
+      <Modal visible={modalVisible} animationType='fade' transparent={true}>
+        <ModalPassaword />
+      </Modal>
 
       <Text style={styles.genText}>{senhaGerada}</Text>
     </View>
@@ -47,20 +54,20 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
-    backgroundColor:"#333",
+    backgroundColor: "#333",
     width: '70%',
     height: 50,
     borderRadius: 8,
     alignItems: 'center',
-    justifyContent:'center'
+    justifyContent: 'center'
   },
-  textBtn:{
-    color:'#fff',
+  textBtn: {
+    color: '#fff',
     fontWeight: 'bold',
     fontSize: 18,
 
   },
-  genText:{
-    color:'#000'
+  genText: {
+    color: '#000'
   },
 });
---
+++ geradorSenhas/src/components/modal/index.js
@@ -0,0 +1,93 @@
+import { View, Text, StyleSheet, Pressable, TouchableOpacity } from "react-native";
+
+export function ModalPassaword() {
+    return (
+
+        <View style={styles.container}>
+            <View style={styles.content}><Text style={styles.title}>Senha geral</Text>
+
+                <Pressable style={styles.innerPassword}>
+                    <Text style={styles.text}>123456789</Text>
+                </Pressable>
+
+                <View style={styles.btnArea}>
+                    
+                    <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btnText}>voltar</Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity style={styles.btnSave}>
+                    <Text style={styles.btnTextSave}>Salvar Senha</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "rgba(24,24,24, 0.6)",
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    content: {
+        backgroundColor: '#FFF',
+        width: "85%",
+        paddingTop: 24,
+        paddingBottom: 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#000',
+        paddingBottom: 18,
+    },
+
+    innerPassword: {
+        backgroundColor: "#E6E6E6",
+        width: "90%",
+        padding: 14,
+        borderRadius: 8,
+
+    },
+    text: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+
+    },
+    btnArea: {
+        flexDirection:"row",
+        width: "90%",
+        alignItems: "center",
+        justifyContent:"space-between",
+        marginTop: 14,
+
+    },
+    btnTextSave:{
+        color: '#FFF',
+    },
+    btnText:{
+
+    },
+    btn:{
+        flex:1,
+        alignItems:'center',
+        padding: 8,
+
+    },
+    btnSave:{
+        flex:1,
+        alignItems:'center',
+        padding: 8,
+        backgroundColor:"#333",
+        borderRadius: 8,
+
+    },
+})
